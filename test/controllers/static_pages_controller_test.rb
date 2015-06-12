1275261e0b2406d541db8016476581781fb1ebbd
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-06-12 18:39:45 -0400

    Much needed commit

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,20 +4,27 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Snicker"
+    assert_select "title", "Snickr"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Snicker"
+    assert_select "title", "Help | Snickr"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Snicker"
+    assert_select "title", "About | Snickr"
   end
+
+  test 'should get contact' do
+    get :contact
+    assert_response :success
+    assert_select 'title', 'Contact | Snickr'
+  end
+
 end
 
 
