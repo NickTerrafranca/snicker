168edccd52a846106fdb78b8c958d906a8df6e69
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-06-28 14:39:15 -0400

    Adding login/out functionality and tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # binding.pry
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      # stuff
+      flash.now[:danger] = "Invalid email or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
