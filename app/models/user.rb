4e81683d3fd09774a6f9b50a403666baf638a8e0
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-07-29 17:31:28 -0400

    Added the ability to forget user cookies and log out users with persistent cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,10 +24,14 @@ class User < ActiveRecord::Base
 
   def remember
     self.remember_token = User.new_token
-    User.update_attribute(remember_token, User.digest(remember_token))
+    update_attribute(remember_token, User.digest(remember_token))
   end
 
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
