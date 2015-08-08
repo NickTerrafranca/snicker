388fd90299bfbb7c6ff93cc31e0feb42497020ef
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-08-08 15:47:15 -0400

    Fixed test of user edit and update authorization Cleaned up some test code

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,7 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user = users(:nicholas)
   end
 
-  test "unsuccessful edit" do
+  test 'unsuccessful edit' do
     log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
@@ -14,13 +14,13 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  test "successful edit" do
+  test 'successful edit' do
     log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
-    name  = "Foo Bar"
-    email = "foo@bar.com"
-    patch user_path(@user), user: { name: name, email: email, password: "", password_confirmation: "" }
+    name  = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), user: { name: name, email: email, password: '', password_confirmation: '' }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
