a61f5e41efce86c64ead3072154a046d50a46b7b
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-08-11 18:29:53 -0400

    Added users index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,17 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'help'      => 'static_pages#help'
+  get 'help' => 'static_pages#help'
 
-  get 'about'     => 'static_pages#about'
+  get 'about' => 'static_pages#about'
 
-  get 'contact'   => 'static_pages#contact'
+  get 'contact' => 'static_pages#contact'
 
-  get 'signup'    => 'users#new'
+  get 'signup' => 'users#new'
 
-  get 'login'     => 'sessions#new'
+  get 'login' => 'sessions#new'
 
-  post 'login'    => 'sessions#create'
+  post 'login' => 'sessions#create'
 
   delete 'logout' => 'sessions#destroy'
 
