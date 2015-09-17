73766e371dcf20b29b70b221403ba025f44f484d
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-09-17 17:47:18 -0400

    Cleaning up extraneous comments Added test for the existence and deletion of friendly forwarding url is stored in session[:forwarding_url]

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -14,20 +14,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
 
-  # this may need to be a part of the test below
-  # test "successful edit with friendly forwarding" do
-  #   get edit_user_path(@user)
-  #   log_in_as(@user)
-  #   assert_redirected_to edit_user_path(@user)
-  # end
-
-  # test 'successful edit' do
-  #   log_in_as(@user)
-  #   get edit_user_path(@user)
-  #   assert_template 'users/edit'
-
   test "successful edit with friendly forwarding" do
     get edit_user_path(@user)
+      assert_not_nil session[:forwarding_url]
     log_in_as(@user)
     assert_redirected_to edit_user_path(@user)
     name  = 'Foo Bar'
@@ -35,6 +24,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     patch user_path(@user), user: { name: name, email: email, password: '', password_confirmation: '' }
     assert_not flash.empty?
     assert_redirected_to @user
+    assert_nil session[:forwarding_url]
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
