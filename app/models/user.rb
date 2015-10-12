433d9998cfd7d5b9d38fd78ec749866525edc258
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-10-12 15:58:58 -0400

    Added account_activation utility In the process of setting up mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-
-  before_save { self.email = email.downcase }
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 155 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
@@ -26,7 +26,6 @@ class User < ActiveRecord::Base
   end
 
   class << self
-
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
@@ -36,6 +35,14 @@ class User < ActiveRecord::Base
     def new_token
       SecureRandom.urlsafe_base64
     end
-
   end
+
+  private
+    def downcase_email
+      self.email = email.downcase
+    end
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
