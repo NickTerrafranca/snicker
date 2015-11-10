7b47b02ccbf24a8c817f298ee3fe173e73d6340c
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-11-10 17:58:11 -0500

    Re-factored and added tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,10 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, user: { name:  "Example User",
-                               email: "user@example.com",
-                               password:              "password",
-                               password_confirmation: "password" }
+      post users_path, user: { name:  "Example User", email: "user@example.com", password: "password", password_confirmation: "password" }
     end
     assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
@@ -33,6 +30,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     # Try to log in before activation.
     log_in_as(user)
     assert_not is_logged_in?
+    # Index page
+    # Log in as valid user.
+    log_in_as(users(:nicholas))
+    # Unactivated user is on the second page
+    get users_path, page: 2
+    assert_no_match user.name, response.body
+    # Profile page
+    get user_path(user)
+    assert_redirected_to root_url
+    # Log out valid user.
+    delete logout_path
     # Invalid activation token
     get edit_account_activation_path("invalid token")
     assert_not is_logged_in?
