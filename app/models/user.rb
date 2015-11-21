5d5239ded1e39fe5abc030d5002582d9e7b48aef
Nicholas Terrafranca <nterrafranca@gmail.com>
2015-11-21 14:58:24 -0500

    Added micropost migration, basic model validations and associations as well as tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
-
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-
+  has_many :microposts
   before_save   :downcase_email
   before_create :create_activation_digest
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 155 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
